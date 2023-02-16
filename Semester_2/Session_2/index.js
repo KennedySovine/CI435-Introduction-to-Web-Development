34c5406e74abc0958ba2a70b0f16e573cb921250
KennedySovine <89476714+KennedySovine@users.noreply.github.com>
2023-02-16 13:35:35 +0000

    Week 2 - Basic Javascript
    We literally have just done console.log. This is boring. Help me. All of the work done is stuff required for the tutorial workbook.

---
+++ Semester_2/Session_2/index.js
@@ -0,0 +1,11 @@
+console.log("Hello World!"); 
+
+var a = 5;
+console.log(a);
+var b = 3;
+console.log(a*b);
+console.log((a+b) % 3);
+var s1 = "Hello";
+var s2 = "Kennedy";
+console.log(s1+" "+s2);
+console.log(a + " squared is " + (a * a));
