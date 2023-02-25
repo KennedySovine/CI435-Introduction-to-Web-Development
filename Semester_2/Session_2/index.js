e1aad9bb158233b8b0c05ea7bf8f98a5eea4ab0d
KennedySovine <89476714+KennedySovine@users.noreply.github.com>
2023-02-25 13:00:08 +0000

    Update of week 2

--- Semester_2/Session_2/index.js
+++ Semester_2/Session_2/index.js
@@ -3,11 +3,11 @@ console.log("Hello World!");
 // ---------
 
 var a = 5;
-console.log(a);
+console.log(a); //5
 var b = 3;
-console.log(a*b);
-console.log((a+b) % 3);
+console.log(a*b); //15
+console.log((a+b) % 3); //5
 var s1 = "Hello";
 var s2 = "Kennedy";
-console.log(s1+" "+s2);
-console.log(a + " squared is " + (a * a));
+console.log(s1+" "+s2); //"Hello Kennedy"
+console.log(a + " squared is " + (a * a)); //"5 squared is 25"
